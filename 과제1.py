1d4353ecb797b8bc7e534d0c05fe176e15dc3b68
haeunsong <62704334+haeunsong@users.noreply.github.com>
2020-04-29 23:59:29 +0900

    과제1

---
+++ 과제1.py
@@ -0,0 +1,5 @@
+#과제 1
+name=input()
+number=input()
+print("제 이름은 %s입니다.\n제 전화번호는 %s입니다."%(name,number))
+
---
+++ 과제2.py
@@ -0,0 +1,15 @@
+# 과제 2
+import re
+
+a = 'My  naMe  is  Son  Chang  Ha:  my  pin  is  000125-3!!!!!!.'
+
+try1 = " ".join(a.split())
+try2 = try1.replace(":",",")
+try3 = re.sub('[!]',"",try2)
+w = "naMe".lower()
+ww = "m".upper()
+try4 = try3[:3]
+try5 = try3[7:25]
+try6 = try3[26:]
+
+print(try4+w+try5+ww+try6)
